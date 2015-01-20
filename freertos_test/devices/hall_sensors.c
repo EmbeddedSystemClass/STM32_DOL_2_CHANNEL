46622e65595770e8973d4f527b5d509e0f6c107d
nowhard <nowhard@mail.ru>
2015-01-20 11:24:57 +0300

    Первый коммит магнитного ДОЛа

---
+++ freertos_test/devices/hall_sensors.c
@@ -0,0 +1,19 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_tim.h"
+#include <misc.h>
+
+//Инклуды от FreeRTOS:
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "hall_sensors.h"
+
+void Hall_Sensors_Init(void)
+{
+
+}
---
+++ freertos_test/devices/hall_sensors.h
@@ -0,0 +1,7 @@
+#ifndef HALL_SENSORS_H
+#define HALL_SENSORS_H
+
+void Hall_Sensors_Init(void);
+
+
+#endif
