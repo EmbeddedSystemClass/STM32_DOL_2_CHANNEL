fba118168a9ab383d1440b4b810a5f447f1e0602
nowhard <nowhard@mail.ru>
2015-01-21 10:31:44 +0300

    Вывод 2 фаз

--- freertos_test/devices/hall_sensors.c
+++ freertos_test/devices/hall_sensors.c
@@ -19,26 +19,16 @@
 #include "watchdog.h"
 extern struct Channel  channels[];
 
+static volatile uint8_t sensors_state=0;
 
-#define HALL_SENSORS_PORT    		GPIOC
-#define HALL_SENSORS_PORT_RCC 		RCC_AHB1Periph_GPIOC
-#define HALL_SENSORS_PORT_EXTI 		EXTI_PortSourceGPIOC
+static volatile uint32_t counter=0x80008000;
 
-#define HALL_SENSOR_0_PIN 			GPIO_Pin_0
-#define HALL_SENSOR_1_PIN 			GPIO_Pin_1
-#define HALL_SENSOR_2_PIN 			GPIO_Pin_2
-#define HALL_SENSOR_3_PIN 			GPIO_Pin_3
-
-#define HALL_SENSOR_0_PinSource 	GPIO_PinSource0
-#define HALL_SENSOR_1_PinSource 	GPIO_PinSource1
-#define HALL_SENSOR_2_PinSource 	GPIO_PinSource2
-#define HALL_SENSOR_3_PinSource 	GPIO_PinSource3
 
 void Hall_Process( void *pvParameters );
 
 void Hall_Sensors_Init(void)
 {
-	RCC_AHB1PeriphClockCmd(HALL_SENSORS_PORT_RCC, ENABLE);//
+	RCC_AHB1PeriphClockCmd(HALL_SENSORS_PORT_RCC|ENC_PORT_RCC, ENABLE);//
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 
 	GPIO_InitTypeDef  GPIO_InitStructure;
@@ -48,6 +38,10 @@ void Hall_Sensors_Init(void)
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(HALL_SENSORS_PORT, &GPIO_InitStructure);
 
+	GPIO_InitStructure.GPIO_Pin = ENC_0_PIN|ENC_1_PIN;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(ENC_PORT, &GPIO_InitStructure);
 
 	EXTI_InitTypeDef EXTI_InitStructure;
 
@@ -85,25 +79,25 @@ void Hall_Sensors_Init(void)
 	NVIC_InitTypeDef NVIC_InitStructure;
 
 	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 14;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 7;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 
 	NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 14;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 7;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 
 	NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 14;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 7;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 
 	NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 14;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 7;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
@@ -113,36 +107,43 @@ void Hall_Sensors_Init(void)
 	NVIC_EnableIRQ(EXTI2_IRQn);
 	NVIC_EnableIRQ(EXTI3_IRQn);
 
+	switch(counter&ENC_MASK)
+	{
+		case 0x0:
+		{
+			ENC_PORT->BSRRH=(ENC_0_PIN|ENC_1_PIN);
+		}
+		break;
+
+		case 0x1:
+		{
+			ENC_PORT->BSRRH=ENC_0_PIN;
+			ENC_PORT->BSRRL=ENC_1_PIN;
+		}
+		break;
+
+		case 0x2:
+		{
+			ENC_PORT->BSRRL=ENC_0_PIN;
+			ENC_PORT->BSRRL=ENC_1_PIN;
+		}
+		break;
+
+		case 0x3:
+		{
+			ENC_PORT->BSRRL=ENC_0_PIN;
+			ENC_PORT->BSRRH=ENC_1_PIN;
+		}
+		break;
+	}
+
 	xTaskCreate(Hall_Process,(signed char*)"HALL_PROCESS",128, NULL, tskIDLE_PRIORITY + 1, NULL);
 }
 
-enum
-{
-	HALL_SENSOR_0=0,
-	HALL_SENSOR_1,
-	HALL_SENSOR_2,
-	HALL_SENSOR_3,
-};
 
-enum
-{
-	HALL_STATE_0=0x3,
-	HALL_STATE_1=0x2,
-	HALL_STATE_2=0x0,
-	HALL_STATE_3=0x1,
-};
 
-enum
-{
-	RIGHT=0,
-	LEFT=1,
-};
 
-#define IDR_MASK	0xF
 
-static volatile uint8_t last_sensor=0xFF,direction=RIGHT,sensors_state=0;
-
-static volatile uint32_t counter=0x80008000;
 
 void EXTI0_IRQHandler(void)
 {
@@ -174,6 +175,36 @@ void EXTI0_IRQHandler(void)
 		 		counter++;
 		 	 }
 		 	 break;
+		}
+
+		switch(counter&ENC_MASK)
+		{
+			case 0x0:
+			{
+				ENC_PORT->BSRRH=(ENC_0_PIN|ENC_1_PIN);
+			}
+			break;
+
+			case 0x1:
+			{
+				ENC_PORT->BSRRH=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x2:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x3:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRH=ENC_1_PIN;
+			}
+			break;
 		}
 }
 
@@ -208,6 +239,36 @@ void EXTI1_IRQHandler(void)
 		 	 }
 		 	 break;
 		}
+
+		switch(counter&ENC_MASK)
+		{
+			case 0x0:
+			{
+				ENC_PORT->BSRRH=(ENC_0_PIN|ENC_1_PIN);
+			}
+			break;
+
+			case 0x1:
+			{
+				ENC_PORT->BSRRH=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x2:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x3:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRH=ENC_1_PIN;
+			}
+			break;
+		}
 }
 
 void EXTI2_IRQHandler(void)
@@ -241,6 +302,36 @@ void EXTI2_IRQHandler(void)
 		 	 }
 		 	 break;
 		}
+
+		switch(counter&ENC_MASK)
+		{
+			case 0x0:
+			{
+				ENC_PORT->BSRRH=(ENC_0_PIN|ENC_1_PIN);
+			}
+			break;
+
+			case 0x1:
+			{
+				ENC_PORT->BSRRH=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x2:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x3:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRH=ENC_1_PIN;
+			}
+			break;
+		}
 }
 
 void EXTI3_IRQHandler(void)
@@ -273,6 +364,36 @@ void EXTI3_IRQHandler(void)
 		 		counter++;
 		 	 }
 		 	 break;
+		}
+
+		switch(counter&ENC_MASK)
+		{
+			case 0x0:
+			{
+				ENC_PORT->BSRRH=(ENC_0_PIN|ENC_1_PIN);
+			}
+			break;
+
+			case 0x1:
+			{
+				ENC_PORT->BSRRH=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x2:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRL=ENC_1_PIN;
+			}
+			break;
+
+			case 0x3:
+			{
+				ENC_PORT->BSRRL=ENC_0_PIN;
+				ENC_PORT->BSRRH=ENC_1_PIN;
+			}
+			break;
 		}
 }
 
--- freertos_test/devices/hall_sensors.h
+++ freertos_test/devices/hall_sensors.h
@@ -1,6 +1,39 @@
 #ifndef HALL_SENSORS_H
 #define HALL_SENSORS_H
 
+enum
+{
+	HALL_STATE_0=0x3,
+	HALL_STATE_1=0x2,
+	HALL_STATE_2=0x0,
+	HALL_STATE_3=0x1,
+};
+
+
+#define IDR_MASK	0xF
+#define ENC_MASK 	0x3
+
+#define HALL_SENSORS_PORT    		GPIOC
+#define HALL_SENSORS_PORT_RCC 		RCC_AHB1Periph_GPIOC
+#define HALL_SENSORS_PORT_EXTI 		EXTI_PortSourceGPIOC
+
+#define HALL_SENSOR_0_PIN 			GPIO_Pin_0
+#define HALL_SENSOR_1_PIN 			GPIO_Pin_1
+#define HALL_SENSOR_2_PIN 			GPIO_Pin_2
+#define HALL_SENSOR_3_PIN 			GPIO_Pin_3
+
+#define HALL_SENSOR_0_PinSource 	GPIO_PinSource0
+#define HALL_SENSOR_1_PinSource 	GPIO_PinSource1
+#define HALL_SENSOR_2_PinSource 	GPIO_PinSource2
+#define HALL_SENSOR_3_PinSource 	GPIO_PinSource3
+
+#define ENC_PORT    		GPIOC
+#define ENC_PORT_RCC 		RCC_AHB1Periph_GPIOC
+
+#define ENC_0_PIN 			GPIO_Pin_11
+#define ENC_1_PIN 			GPIO_Pin_12
+
+
 void Hall_Sensors_Init(void);
 
 
